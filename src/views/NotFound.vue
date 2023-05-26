d09740271764176ca28d1349e29a51840a272ad2
Hercule555 <1076297655@qq.com>
2023-05-26 20:21:37 +0800

    404 页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>404,网址有误请重新输入</div>
+    <div>
+      <router-link to="/">返回首页</router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "NotFound",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
