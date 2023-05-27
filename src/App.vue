e63f6ca6c9f67b46cbb167320686ec3456625e3c
Hercule555 <1076297655@qq.com>
2023-05-27 16:08:53 +0800

    Money页面布局

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,13 @@ import router from './router/index';
 
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
