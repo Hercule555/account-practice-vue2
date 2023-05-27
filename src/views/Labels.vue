31b6a9e9d86c56c9eda689f34719a7779404e53c
Hercule555 <1076297655@qq.com>
2023-05-27 16:42:57 +0800

    重复三次布局

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>lab
+  <div class="nav-wrapper">
+    <div class="content">lab</div>
     <Nav />
   </div>
 </template>
@@ -12,4 +13,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+ .nav-wrapper {
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content {
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>sta
+  <div class="nav-wrapper">
+    <div class="content">sta</div >
     <Nav />
   </div>
 </template>
@@ -13,4 +14,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+ .nav-wrapper {
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content {
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
