aeb412ebd03c6fd2eb3f5bdbec35447da89642cf
Hercule555 <1076297655@qq.com>
2023-06-06 15:01:03 +0800

    html 和 css 写好

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,7 @@ body {
   line-height: 1.5;
   font-family: $font-hei;
   color: #333;
+  font-size: 16px;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
+  <div class="layout-wrapper">
+    <div class="content" :class="classPrefix && `${classPrefix}-content` ">
       <slot />
     </div>
     <Nav />
@@ -11,12 +11,13 @@
 
 
 export default {
+  props:['classPrefix'],
   name: "Layout",
 };
 </script>
 
 <style lang="scss" scoped>
-.nav-wrapper {
+.layout-wrapper {
   
   display: flex;
   flex-direction: column;
