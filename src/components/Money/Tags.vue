943f4802bdac10f484929c8f503babc7d099c769
Hercule555 <1076297655@qq.com>
2023-06-06 22:18:48 +0800

    模块化：把Money.vue 分为了4个组件

---
+++ src/components/Money/Tags.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="tags">
+      <div class="new">
+        <button>新增标签</button>
+      </div>
+      <ul class="current">
+        <li>衣</li>
+        <li>食</li>
+        <li>住</li>
+        <li>行</li>
+        <li>衣</li>
+        <li>食</li>
+        <li>住</li>
+        <li>行</li>
+        <li>衣</li>
+        <li>食</li>
+        <li>住</li>
+        <li>行</li>
+        <li>衣</li>
+        <li>食</li>
+        <li>住</li>
+        <li>行</li>
+      </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: 'Tags'
+    }
+</script>
+
+<style lang="scss" scoped>
+.tags {
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column-reverse;
+  font-size: 14px;
+  padding: 16px;
+  > .current {
+    display: flex;
+    flex-wrap: wrap;
+    > li {
+      $h: 24px;
+      height: $h;
+      border-radius: $h/2;
+      padding: 0 16px;
+      margin-right: 12px;
+      margin-top: 4px;
+      font-size: 14px;
+      background-color: #d9d9d9;
+    }
+  }
+  > .new {
+    padding-top: 16px;
+    button {
+      background: transparent;
+      border: none;
+      color: #999;
+      border-bottom: 1px solid;
+      padding: 0 4px;
+    }
+  }
+}
+</style>
