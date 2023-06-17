decac16989e8cf18fa3a5adfd21d512fe2fbb7d8
Hercule555 <1076297655@qq.com>
2023-06-17 18:02:40 +0800

    创建EditLabel 将tag 改为{id，name}

---
+++ src/views/EditLabel.vue
@@ -0,0 +1,17 @@
+<template>
+    <Layout>
+      编辑标签
+    </Layout>
+</template>
+
+<script lang="ts">
+import Layout from '@/components/Layout.vue'
+    export default {
+  components: { Layout },
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
