2c13bb3780acf2b0e6237a74b0e5f23ced7af5ad
Hercule555 <1076297655@qq.com>
2023-06-25 17:42:49 +0800

    将所有操作封装在store里

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,31 +1,22 @@
 type RecordItem = {
-    tags: string[];
-    notes: string;
-    type: string;
-    amount: number;
-    createdAt?: Date;
-  };
+  tags: string[];
+  notes: string;
+  type: string;
+  amount: number;
+  createdAt?: Date;
+};
 
-  type Tag = {
-    id: string
-    name: string
-  }
-  
-  type TagListModel = {
-      data: Tag[]
-      fetch: () => Tag[]
-      create: (name: string) => 'success'| 'duplicated'
-      update: (id:string, name: string) => 'success'| 'duplicated' | 'not found'
-      remove: (id: string) => boolean
-      save: () => void
-  }
+type Tag = {
+  id: string
+  name: string
+}
 
-  interface Window {
-    tagList: Tag[],
-    findTag: (id: string) => Tag | undefined, 
-    createTag: (name: string) => void,
-    removeTag: (id: string) => boolean,
-    updateTag: (id: string, name: string) => 'success'| 'duplicated' | 'not found', 
-    recordList: RecordItem[];
-    createRecord: (record: RecordItem) => void
-  }
+type TagListModel = {
+  data: Tag[]
+  fetch: () => Tag[]
+  create: (name: string) => 'success' | 'duplicated'
+  update: (id: string, name: string) => 'success' | 'duplicated' | 'not found'
+  remove: (id: string) => boolean
+  save: () => void
+}
+
