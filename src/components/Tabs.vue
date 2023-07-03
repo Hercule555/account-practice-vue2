4a8cc79fee703ba0c255c72f19b444da62f3d0af
Hercule555 <1076297655@qq.com>
2023-07-03 14:27:04 +0800

    抽离Tabs.vue 使用在Statistics.vue

---
+++ src/components/Tabs.vue
@@ -0,0 +1,64 @@
+<template>
+  <ul class="tabs">
+    <li
+      v-for="item in dataSource" :key="item.value"
+      @click="select(item)"
+      :class="liClass(item)"
+    >
+      {{ item.text }}
+    </li>
+  </ul>
+</template>
+
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+
+type DataSourceItem = { text: string; value: string };
+
+@Component
+export default class Tabs extends Vue {
+  @Prop({ required: true, type: Array })
+  dataSource!: DataSourceItem[];
+  @Prop(String) readonly value!: string;
+  @Prop(String) classPrefix?: string;
+
+  select(item: DataSourceItem) {
+    this.$emit("update:value", item.value);
+  }
+
+  liClass(item: DataSourceItem) {
+    return {
+      [this.classPrefix + "-tabs-item"]: this.classPrefix,
+      selected: item.value === this.value,
+    };
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.tabs {
+  background: #c4c4c4;
+  display: flex;
+  text-align: center;
+  > li {
+    width: 50%;
+    font-size: 24px;
+    height: 64px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    &.selected::after {
+      content: "";
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 4px;
+      background: #333;
+    }
+  }
+}
+</style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,14 +1,52 @@
 <template>
   <Layout>
-    <p>Statistics</p>
+    <Tabs class-prefix="type" :data-source="typeList"  :value.sync="type"/>
+    <Tabs class-prefix="interval"  :data-source="intervalList"  :value.sync="interval"/>
+    <div>
+      type: {{type}}
+      <br>
+      interval: {{interval}}
+    </div>
   </Layout>
 </template>
 
-<script  lang="ts">
-import Component from "vue-class-component";
 
-export default {
-  name: "Statistics",
-};
+<style lang="scss" scoped>
+::v-deep .type-tabs-item {
+  background: white;
+  color: #626161;
+  &.selected {
+    background: #c4c4c4;
+    color: #333;
+    &::after {
+      display: none;
+    }
+  }
+}
+</style>
+
+
+<script  lang="ts">
+import Types from "@/components/Money/Types.vue";
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+import Tabs from "@/components/Tabs.vue";
+
+@Component({
+  components: { Types, Tabs },
+})
+export default class Statistics extends Vue {
+  type = "-";
+  interval = 'day';
+  intervalList = [
+    { text: "按天", value: "day" },
+    { text: "按周", value: "week" },
+    { text: "按月", value: "month" },
+  ];
+  typeList= [
+    {text: '收入', value:'+'},
+    {text:'支出', value:'-'}
+  ]
+}
 </script>
 
