108c5cf8542bdd8bd6f49097621ecc3b1e015288
Hercule555 <1076297655@qq.com>
2023-07-15 21:41:56 +0800

    提示用户用手机体验，并添加二维码

--- src/main.ts
+++ src/main.ts
@@ -20,8 +20,21 @@ new Vue({
   render: h => h(App)
 }).$mount('#app')
 
-window.onload = function() {
-  setTimeout(function() {
-    window.scrollTo(0,100)
-  },0)
+window.onload = function () {
+  setTimeout(function () {
+    window.scrollTo(0, 100)
+  }, 0)
 }
+
+if (document.documentElement.clientWidth > 500) {
+  window.alert('使用手机体验会更加');
+  const img = document.createElement('img');
+  img.src = '/qrcode.png';
+  img.style.position = 'fixed';
+  img.style.left = '50%';
+  img.style.top = '50%';
+  img.style.transform = 'translate(-50%,-50%)'
+  img.style.boxShadow = '0 0 10px rgba(0,0,0,0.25)'
+  document.body.appendChild(img)
+}
+
