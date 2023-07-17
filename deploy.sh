5e0ed56e30b1593e66cb147a64cd7a416b04c2a8
Hercule555 <1076297655@qq.com>
2023-07-17 14:33:13 +0800

    更新到pages

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ cd dist
 
 git init
 git add -A
-git commit -m '修改二维码bug2'
+git commit -m '修改样式'
 
 # 部署到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
